03764659ad7a0945107508959a30a7b969dd8b94
Frederic Brodbeck <der.froedi@gmail.com>
2021-10-02 22:14:23 +0200

    clean up and comments

--- extension.js
+++ extension.js
@@ -12,16 +12,25 @@ class Extension {
 		// more rows and columns in application overview grid
 		const { appDisplay } = Main.overview._overview.controls;
 		appDisplay._grid._gridModes.push({ columns: 10, rows: 4 });
+		const gridCols = 10; // TODO: get from settings
+		const gridRows = 4;
+		appDisplay._grid._gridModes.push({
+			columns: gridCols,
+			rows: gridRows
+		});
 		appDisplay._grid._setGridMode(appDisplay._grid._gridModes.length - 1);
 
 		// more rows and columns in application folder
+		const folderCols = 8; // TODO: get from settings
+		const folderRows = 4;
+		// https://gitlab.gnome.org/GNOME/gnome-shell/-/blob/main/js/ui/appDisplay.js
 		AppDisplay.FolderGrid = GObject.registerClass(
 			class FolderGrid extends IconGrid.IconGrid {
 				_init() {
 					super._init({
 						allow_incomplete_pages: false,
-						columns_per_page: 8,
-						rows_per_page: 4,
+						columns_per_page: folderCols,
+						rows_per_page: folderRows,
 						page_halign: Clutter.ActorAlign.CENTER,
 						page_valign: Clutter.ActorAlign.CENTER,
 					});
